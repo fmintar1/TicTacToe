3e0b371cdf0d151dfb7d0e47d24573eca7b41e3c
Freddy <freddy@Zipcoders-MacBook-Pro-7.local>
2022-11-05 17:10:46 -0400

    More satisfying looking

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,113 +4,168 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
-
+    boolean result = false;
     Character[][] matrix;
     public Board(Character[][] matrix) {
         this.matrix = matrix;
     }
 
-    public Boolean isInFavorOfX () {
-        if (matrix[0][0].equals('X') &&
-                matrix[0][1].equals('X') &&
-                matrix[0][2].equals('X')) {
-            return true;
-        }
-        if (matrix[1][0].equals('X') &&
-                matrix[1][1].equals('X') &&
-                matrix[1][2].equals('X')) {
-            return true;
-        }
-        if (matrix[2][0].equals('X') &&
-                matrix[2][1].equals('X') &&
-                matrix[2][2].equals('X')) {
-            return true;
-        }
-        if (matrix[0][0].equals('X') &&
-                matrix[1][1].equals('X') &&
-                matrix[2][2].equals('X')) {
-            return true;
-        }
-        if (matrix[0][0].equals('X') &&
-                matrix[1][0].equals('X') &&
-                matrix[2][0].equals('X')) {
-            return true;
-        }
-        if (matrix[0][1].equals('X') &&
-                matrix[1][1].equals('X') &&
-                matrix[2][1].equals('X')) {
-            return true;
-        }
-        if (matrix[0][2].equals('X') &&
-                matrix[1][2].equals('X') &&
-                matrix[2][2].equals('X')) {
-            return true;
-        }
-        if (matrix[0][2].equals('X') &&
-                matrix[1][1].equals('X') &&
-                matrix[2][0].equals('X')) {
-            return true;
+    public Boolean checkHorizontal(char value) {
+        for (int y = 0; y < matrix.length; y++) {
+            for (int x = 0; x < matrix.length; x++) {
+                if (matrix[y][x] != value) {
+                    break;
+                } else if (x == matrix.length - 1) {
+                    return result = true;
+                }
+            }
         }
         return false;
     }
-    public Boolean isInFavorOfO () {
-        if (matrix[0][0].equals('O') &&
-                matrix[0][1].equals('O') &&
-                matrix[0][2].equals('O')) {
-            return true;
-        }
-        if (matrix[1][0].equals('O') &&
-                matrix[1][1].equals('O') &&
-                matrix[1][2].equals('O')) {
-            return true;
-        }
-        if (matrix[2][0].equals('O') &&
-                matrix[2][1].equals('O') &&
-                matrix[2][2].equals('O')) {
-            return true;
-        }
-        if (matrix[0][0].equals('O') &&
-                matrix[1][1].equals('O') &&
-                matrix[2][2].equals('O')) {
-            return true;
-        }
-        if (matrix[0][0].equals('O') &&
-                matrix[1][0].equals('O') &&
-                matrix[2][0].equals('O')) {
-            return true;
-        }
-        if (matrix[0][1].equals('O') &&
-                matrix[1][1].equals('O') &&
-                matrix[2][1].equals('O')) {
-            return true;
-        }
-        if (matrix[0][2].equals('O') &&
-                matrix[1][2].equals('O') &&
-                matrix[2][2].equals('O')) {
-            return true;
-        }
-        if (matrix[0][2].equals('O') &&
-                matrix[1][1].equals('O') &&
-                matrix[2][0].equals('O')) {
-            return true;
+    public boolean checkVertical(char value) {
+        for (int x = 0; x < matrix.length; x++) {
+            for (int y = 0; y < matrix.length; y++) {
+                if (matrix[y][x] != value) {
+                    break;
+                } else if (y == matrix.length - 1) {
+                    return result = true;
+                }
+            }
         }
         return false;
+    }
+    public boolean checkDiagonal(char value) {
+        for (int x = 0; x < matrix.length; x++) {
+            if (matrix[x][x] != value) {
+                break;
+            } else if (x == matrix.length-1) {
+                return result = true;
+            }
+        }
+        return false;
+    }
+    public boolean checkInverseDiagonal(char value) {
+        for (int x = 0; x < matrix.length; x++) {
+            if (matrix[x][(matrix.length-1)-x] != value) {
+                break;
+            } else if (x == matrix.length-1) {
+                return result = true;
+            }
+        }
+        return false;
+    }
+
+    public Boolean isInFavorOfX () {
+        checkHorizontal('X');
+        checkVertical('X');
+        checkDiagonal('X');
+        checkInverseDiagonal('X');
+        return result;
+    }
+//        if (matrix[0][0].equals('X') &&
+//                matrix[0][1].equals('X') &&
+//                matrix[0][2].equals('X')) {
+//            return true;
+//        }
+//        if (matrix[1][0].equals('X') &&
+//                matrix[1][1].equals('X') &&
+//                matrix[1][2].equals('X')) {
+//            return true;
+//        }
+//        if (matrix[2][0].equals('X') &&
+//                matrix[2][1].equals('X') &&
+//                matrix[2][2].equals('X')) {
+//            return true;
+//        }
+//        if (matrix[0][0].equals('X') &&
+//                matrix[1][1].equals('X') &&
+//                matrix[2][2].equals('X')) {
+//            return true;
+//        }
+//        if (matrix[0][0].equals('X') &&
+//                matrix[1][0].equals('X') &&
+//                matrix[2][0].equals('X')) {
+//            return true;
+//        }
+//        if (matrix[0][1].equals('X') &&
+//                matrix[1][1].equals('X') &&
+//                matrix[2][1].equals('X')) {
+//            return true;
+//        }
+//        if (matrix[0][2].equals('X') &&
+//                matrix[1][2].equals('X') &&
+//                matrix[2][2].equals('X')) {
+//            return true;
+//        }
+//        if (matrix[0][2].equals('X') &&
+//                matrix[1][1].equals('X') &&
+//                matrix[2][0].equals('X')) {
+//            return true;
+//        }
+//        return false;
+//    }
+    public Boolean isInFavorOfO () {
+        checkHorizontal('O');
+        checkVertical('O');
+        checkDiagonal('O');
+        checkInverseDiagonal('O');
+        return result;
+//        if (matrix[0][0].equals('O') &&
+//                matrix[0][1].equals('O') &&
+//                matrix[0][2].equals('O')) {
+//            return true;
+//        }
+//        if (matrix[1][0].equals('O') &&
+//                matrix[1][1].equals('O') &&
+//                matrix[1][2].equals('O')) {
+//            return true;
+//        }
+//        if (matrix[2][0].equals('O') &&
+//                matrix[2][1].equals('O') &&
+//                matrix[2][2].equals('O')) {
+//            return true;
+//        }
+//        if (matrix[0][0].equals('O') &&
+//                matrix[1][1].equals('O') &&
+//                matrix[2][2].equals('O')) {
+//            return true;
+//        }
+//        if (matrix[0][0].equals('O') &&
+//                matrix[1][0].equals('O') &&
+//                matrix[2][0].equals('O')) {
+//            return true;
+//        }
+//        if (matrix[0][1].equals('O') &&
+//                matrix[1][1].equals('O') &&
+//                matrix[2][1].equals('O')) {
+//            return true;
+//        }
+//        if (matrix[0][2].equals('O') &&
+//                matrix[1][2].equals('O') &&
+//                matrix[2][2].equals('O')) {
+//            return true;
+//        }
+//        if (matrix[0][2].equals('O') &&
+//                matrix[1][1].equals('O') &&
+//                matrix[2][0].equals('O')) {
+//            return true;
+//        }
+//        return false;
     }
     public Boolean isTie () {
-        if (isInFavorOfO().equals(false) &&
-                isInFavorOfX().equals(false)) {
-            return true;
+        if (isInFavorOfO() == false &&
+        isInFavorOfX() == false) {
+            return result = true;
+        } else {
+            return result = false;
         }
-        return false;
     }
     public String getWinner () {
-        String result = "";
-        if(isInFavorOfX().equals(true)) {
-            result = "X";
+        if(isInFavorOfX() == true) {
+            return "X";
+        } else if (isInFavorOfO() == true) {
+            return "O";
         }
-        if(isInFavorOfO().equals(true)) {
-            result = "O";
-        }
-        return result;
+        return "";
     }
 }
